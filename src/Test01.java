8f4d934046fd004b969c704e8c7a504fd9e236f7
nicoleshiner <nicoleshiner@localhost>
2022-03-20 10:24:23 +0100

    first commit

---
+++ src/Test01.java
@@ -0,0 +1,17 @@
+
+public class Test01 {
+
+	public static void main(String[] args) {
+
+
+		boolean isTired = true;
+		
+		if(isTired) {
+			System.out.println("More sleep please.");
+		}
+		else {
+			System.out.println("Go go! Be productive");
+		}
+	}
+
+}
